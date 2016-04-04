63cfc0b3040c97f2caeefc314c1d644547f90094
jixj <jixj@lenovo.com>
2016-04-04 15:24:56 +0800

    fix build error
    sdcardfs is linked to device/mt/sdcardfs, ../ is device/mt
    
    Change-Id: I8da52f3eeb9336283649811cf6ce05d9778c9dcf

--- main.c
+++ main.c
@@ -23,7 +23,11 @@
 #include <linux/module.h>
 #include <linux/types.h>
 #include <linux/parser.h>
-#include "../internal.h"
+
+/*
+ * dcache.c
+ */
+extern struct dentry *__d_alloc(struct super_block *, const struct qstr *);
 
 enum {
 	Opt_uid, 
